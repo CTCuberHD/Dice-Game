87587629dab5974c42e0ac82e92629cf8bdd9f94
CTCuberHD <59776094+CTCuberHD@users.noreply.github.com>
2020-03-16 14:28:34 +0000

    Finsihed project
    Added play again button, javascript functions for calculating the winner and added on click event to Button.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,19 @@
-var diceRollOne = randomiser(diceRollOne);
-var randomImageSourceOne = createImageSource(randomImageSourceOne, diceRollOne);
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSourceOne);
+var diceRollOne;
+var diceRollTwo;
 
-var diceRollTwo = randomiser(diceRollTwo);
-var randomImageSourceTwo = createImageSource(randomImageSourceTwo, diceRollTwo);
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSourceTwo);
+function calculateDiceOne()
+{
+   diceRollOne = randomiser(diceRollOne);
+   var randomImageSourceOne = createImageSource(randomImageSourceOne, diceRollOne);
+   document.querySelectorAll("img")[0].setAttribute("src", randomImageSourceOne);
+}
+
+function calculateDiceTwo()
+{
+   diceRollTwo = randomiser(diceRollTwo);
+   var randomImageSourceTwo = createImageSource(randomImageSourceTwo, diceRollTwo);
+   document.querySelectorAll("img")[1].setAttribute("src", randomImageSourceTwo);
+}
 
 function randomiser(x)
 {
@@ -16,4 +25,31 @@ function createImageSource(src, roll)
 {
    src = "./images/dice" + roll + ".png";
    return src;
+}
+
+function calculateWinner(x, y)
+{
+   if(x > y)
+   {
+      document.querySelector("h1").textContent = "Player 1 Wins!";
+   }
+
+   else if(y > x)
+   {
+      document.querySelector("h1").textContent = "Player 2 Wins!";
+   }
+
+   else
+   {
+      document.querySelector("h1").textContent = "It's a Draw!";
+   }
+}
+
+
+function start()
+{
+   calculateDiceOne();
+   calculateDiceTwo();
+   calculateWinner(diceRollOne, diceRollTwo);
+   document.querySelector("button").textContent = "Play Again"
 }
